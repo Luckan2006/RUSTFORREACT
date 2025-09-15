6d448774fa841a5aee37f3375f827a5a3dfd5fba
Luckan2006 <lukas.pmail.se@gmail.com>
2025-09-15 09:15:38 +0200

    added axum

---
+++ server/target/debug/build/proc-macro2-c53cab9167ce2ac9/build_script_build-c53cab9167ce2ac9.d
@@ -0,0 +1,5 @@
+C:\Users\06lukper\Documents\GitHub\RUSTFORREACT\server\target\debug\build\proc-macro2-c53cab9167ce2ac9\build_script_build-c53cab9167ce2ac9.d: C:\Users\06lukper\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.101\build.rs
+
+C:\Users\06lukper\Documents\GitHub\RUSTFORREACT\server\target\debug\build\proc-macro2-c53cab9167ce2ac9\build_script_build-c53cab9167ce2ac9.exe: C:\Users\06lukper\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.101\build.rs
+
+C:\Users\06lukper\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.101\build.rs:
