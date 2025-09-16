586bc555a56e60d8e733b6183252a4d6309fa9b7
derRoteBaron <Arvid.Borgeson@me.com>
2025-09-16 11:03:23 +0200

    penis bög

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,9 +2,14 @@ use axum::{routing::get, Router};
 
 
 
+#[tokio::main]
 
 
-struct MyStruct {
 
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener =tokio::net::TcpListener::bind("
 }
+
 
